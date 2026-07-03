f56602e038e10330583df3ae76216016df151b1b
ronak0903 <ronaksolanki@bitcot.com>
2026-07-03 11:57:13 +0530

    Bypass DescribeSubnets SCP by letting AWS choose default subnet

--- devsecops-terraform/main.tf
+++ devsecops-terraform/main.tf
@@ -2,13 +2,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
 module "vpc" {
   count  = var.create_vpc ? 1 : 0
   source = "./modules/vpc"
@@ -31,7 +24,9 @@ module "ec2" {
   key_name      = var.key_name
   ami_id        = var.ami_id
 
-  subnet_id = var.create_vpc ? module.vpc[0].public_subnet_id : element(data.aws_subnets.default.ids, 0)
+  # If create_vpc is false, pass null to let AWS automatically select the default subnet.
+  # This avoids calling DescribeSubnets, which is blocked by SCP in restricted accounts.
+  subnet_id = var.create_vpc ? module.vpc[0].public_subnet_id : null
   vpc_id    = var.create_vpc ? module.vpc[0].vpc_id : data.aws_vpc.default.id
 
   ecr_url   = var.create_ecr ? module.ecr[0].repository_url : ""
